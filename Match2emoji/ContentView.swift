f839b9b4c42722f37e4ba242c8fd11500249ca9d
Vadim Archer <vadim.archer@gmail.com>
2022-07-20 16:32:21 +0300

    Initial Commit

---
+++ Match2emoji/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Match2emoji
+//
+//  Created by Vadim Archer on 20.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Match2emoji/Match2emojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Match2emojiApp.swift
+//  Match2emoji
+//
+//  Created by Vadim Archer on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Match2emojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
